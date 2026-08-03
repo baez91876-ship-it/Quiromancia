7d1fb6e501534502c24a13981cb86176dcfe6ebc
baez91876-ship-it <baez91876@gmail.com>
2026-08-03 11:09:52 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => res.send('Bienvenidos a la Quiromancia!'))
+
+const usuarios = [
+]
+
+
+const lineaspalmares = [
+
+]
+const perfilnumerologico = {
+
+}
+
+const lecturanumero = {
+}
+const alma = {
+
+}
+const interpretacion = {
+
+}
+const  destino = {
+  
+}
+
