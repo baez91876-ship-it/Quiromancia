ac1273a16a10dcc82875465cd618d0aeb7cb456f
baez91876-ship-it <baez91876@gmail.com>
2026-08-06 20:37:42 -0500

    Fix MongoDB connection and configure repository

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+import 'dotenv/config'
 import express from 'express'
+import { cnxmongo } from './database/cnxmongo.js'
+
 const app = express()
 
 app.get('/', (req, res) => res.send('Bienvenidos a la Quiromancia!'))
+
+const PORT = process.env.PORT || 3000
+
+await cnxmongo()
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en http://localhost:${PORT}`)
+})
 
 const usuarios = [
 ]
@@ -16,6 +27,7 @@ const perfilnumerologico = {
 
 const lecturanumero = {
 }
+
 const alma = {
 
 }
